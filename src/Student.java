d99de8b2d3d0623ae6c466d20e56a3353d9680fe
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-08-30 10:38:32 -0500

    finished constructors

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,13 @@
 public class Student
 {
     private String name;
+    private String idNum;
     private double gpa;
-    private String id;
 
-    public Student(String n, String idNum, double gpaNum)
+    public Student(String n, String id, double gpaNum)
     {
         this.name = n;
-        this.id = idNum;
+        this.idNum = id;
         this.gpa = gpaNum;
     }
 }
