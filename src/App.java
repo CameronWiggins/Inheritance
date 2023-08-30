f118dd08a503c80dc1b3fac5887a8271926a452a
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-08-30 10:56:02 -0500

    Testing Constructors

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Student s1 = new Student("Kelsey Omaro", "11563", 3.854);
+        Student s2 = new Student("Jeff Reidus", "99872", 2.731);
+        GradStudent gs1 = new GradStudent("Margie Neff", "12009", 4.0, 122, "8:00 - 2:00");
+        GradStudent gs2 = new GradStudent("Nathan Kelly", "77743", 3.918, 213, "12:00 - 6:00");
+
+        
+
     }
 }
