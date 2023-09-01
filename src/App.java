78f1b68c15c6c4faa4b61af5d5b732370395c595
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-09-01 10:09:44 -0500

    final testing

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Student s1 = new Student("Kelsey Omaro", "11563", 3.854);
@@ -5,10 +7,20 @@ public class App {
         GradStudent gs1 = new GradStudent("Margie Neff", "12009", 4.0, 122, "8:00 - 2:00");
         GradStudent gs2 = new GradStudent("Nathan Kelly", "77743", 3.918, 213, "12:00 - 6:00");
 
-        s1.setID("22567");
         s2.setGPA(3.533);
         gs1.setName("Margie Olson");
-        gs2.set
+        
+        ArrayList<Student> list = new ArrayList<>();
+
+        list.add(s1);
+        list.add(s2);
+        list.add(gs1);
+        list.add(gs2);
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            System.out.println(list.get(i));
+        }
 
 
 
